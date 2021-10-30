f6d3d320a7905ebe1371bc11b63583e92a179c39
Felice Varriano <felice.varriano@nserc-crsng.gc.ca>
2021-10-30 14:03:21 -0400

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, Text, View } from "react-native";
+import loginscreen from "./screens/loginscreen";
+
+const Stack = createNativeStackNavigator();
+// headerTintColor colors any icons put into the banner as white.
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: { color: "white" },
+  headerTintColor: "white",
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name="Login" component={loginscreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDAqD30xgu5q08Dzyw_a0khGYkM05784LE",
+  authDomain: "signal-clone-yt-e026c.firebaseapp.com",
+  projectId: "signal-clone-yt-e026c",
+  storageBucket: "signal-clone-yt-e026c.appspot.com",
+  messagingSenderId: "850915174214",
+  appId: "1:850915174214:web:cfd6c372520035bed32747",
+};
